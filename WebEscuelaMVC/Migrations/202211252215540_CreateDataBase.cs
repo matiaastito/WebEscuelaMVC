957064d75f88646026445362a413ad74fafa4d8f
matiaastito <matias.tito27@gmail.com>
2022-11-25 19:17:27 -0300

    Create database

---
+++ WebEscuelaMVC/Migrations/202211252215540_CreateDataBase.cs
@@ -0,0 +1,27 @@
+﻿namespace WebEscuelaMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDataBase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Aulas",
+                c => new
+                    {
+                        AulaId = c.String(nullable: false, maxLength: 128),
+                        Numero = c.String(nullable: false),
+                        Estado = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.AulaId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Aulas");
+        }
+    }
+}
